7a7de0b32bdaa1094f209839d42e13d02a22798b
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-06-02 16:58:00 +0900

    :new: slash command を受け取れるように変更

--- src/functions/practice/index.ts
+++ src/functions/practice/index.ts
@@ -1,29 +1,64 @@
-const { WebClient } = require('@slack/client')
+import { WebClient } from "@slack/client"
+var AWS = require('aws-sdk');
+var qs = require('querystring');
+var url = require('url');
+var https = require('https');
 
 const handler = (event, context, callback) => {
+
+    console.log(context);
+    console.log('body');
+    console.log(event.body)
+    const params = qs.parse(event.body);
+    console.log(params)
+
+    // 環境変数からTOKENと投稿チャンネルを取得
     const { SLACK_TOKEN, SLACK_CHANEL } = process.env
     if (!SLACK_TOKEN || !SLACK_CHANEL) return callback(new Error('slack token or channel is undefined'))
     const { message, attachments } = event
     if (!message) return callback(new Error('Message is undefined.'))
 
-    const web = new WebClient(SLACK_TOKEN)
-    const param = {
-        channel: SLACK_CHANEL,
-        text: message,
-        attachments: attachments!
-    }
-    if (attachments) param.attachments = attachments
-    web.chat.postMessage(param)
-        .then((res) => {
-            return callback(null, {
-                event,
-                result: res.ts
-            })
-        })
-        .catch(error => {
-            console.log(error)
-            return callback(error)
-        })
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            challenge: 'ok',
+        }),
+    };
+
+    callback(null, response);
+
+    // WebClientをTokenからインスタンス生成
+    // const web = new WebClient(SLACK_TOKEN)
+    // var lambda = new AWS.Lambda();
+    // var options = {
+    //     FunctionName: event.invokedFunctionArn,
+    //     InvocationType: 'Event',
+    //     Payload: JSON.stringify(params)
+    // };
+    // lambda.invoke(options, (err, data) => {
+    //     if (err) {
+    //         console.log(err, err.stack);
+    //         context.fail("Command failed.");
+    //     } else {
+    //         context.succeed("Command accepted.");
+    //     }
+    // });  
+    // const param = {
+    //     statusCode: 200,
+    //     channel: SLACK_CHANEL,
+    //     text: 'Hello',
+    // }
+    // web.chat.postMessage(param)
+    //     .then((res) => {
+    //         return callback(null, {
+    //             event,
+    //             result: res.ts
+    //         })
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //         return callback(error)
+    //     })
 }
 
 module.exports.handler = handler
