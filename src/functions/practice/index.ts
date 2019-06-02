16d6d684f7721cc68290da8b57e7a81260a4724c
Kazuya_Nagase <al15065@shibaura-it.ac.jp>
2019-06-02 17:32:29 +0900

    :new: 200返すように修正

--- src/functions/practice/index.ts
+++ src/functions/practice/index.ts
@@ -15,50 +15,18 @@ const handler = (event, context, callback) => {
     // 環境変数からTOKENと投稿チャンネルを取得
     const { SLACK_TOKEN, SLACK_CHANEL } = process.env
     if (!SLACK_TOKEN || !SLACK_CHANEL) return callback(new Error('slack token or channel is undefined'))
-    const { message, attachments } = event
-    if (!message) return callback(new Error('Message is undefined.'))
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            challenge: 'ok',
-        }),
-    };
+    // なにか非同期処理
+    (async () => {
+        const delay = (time) => { return new Promise((res) => { return setTimeout(() => { return res() }, time) }) };
+        await delay(15 * 1000);
+    })();
 
-    callback(null, response);
+    callback(null, {
+        statusCode: '200',
+    });
 
-    // WebClientをTokenからインスタンス生成
-    // const web = new WebClient(SLACK_TOKEN)
-    // var lambda = new AWS.Lambda();
-    // var options = {
-    //     FunctionName: event.invokedFunctionArn,
-    //     InvocationType: 'Event',
-    //     Payload: JSON.stringify(params)
-    // };
-    // lambda.invoke(options, (err, data) => {
-    //     if (err) {
-    //         console.log(err, err.stack);
-    //         context.fail("Command failed.");
-    //     } else {
-    //         context.succeed("Command accepted.");
-    //     }
-    // });  
-    // const param = {
-    //     statusCode: 200,
-    //     channel: SLACK_CHANEL,
-    //     text: 'Hello',
-    // }
-    // web.chat.postMessage(param)
-    //     .then((res) => {
-    //         return callback(null, {
-    //             event,
-    //             result: res.ts
-    //         })
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //         return callback(error)
-    //     })
+    console.log('huga')
 }
 
 module.exports.handler = handler
